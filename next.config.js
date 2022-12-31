84d495d945d5f3298d570b378939d5deaeec7d21
deewebgr <119700877+deewebgr@users.noreply.github.com>
2022-12-31 18:39:24 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withTM = require('next-transpile-modules')([
+  '@stripe/firestore-stripe-payments',
+]) // pass the modules you would like to see transpiled
 
-module.exports = nextConfig
+module.exports = withTM({
+  reactStrictMode: true,
+  images: {
+    domains: ['rb.gy', 'image.tmdb.org'],
+  },
+  
+  env: {
+    customKey: 'NEXT_PUBLIC_FIREBASE_API_KEY',
+    customKey: 'NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN',
+    customKey: 'NEXT_PUBLIC_FIREBASE_PROJECT_ID',
+    customKey: 'NEXT_PUBLIC_FIREBASE_STORAGE',
+    customKey: 'NEXT_PUBLIC_FIREBASE_MID',
+    customKey: 'NEXT_PUBLIC_FIREBASE_APP_ID',
+    customKey: 'NEXT_PUBLIC_FIREBASE_MEASURE_ID',
+  },
+})
